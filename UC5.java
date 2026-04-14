25408a4c65a2696c27bd16d61d4f6d27e48a39e1
Ananya Dubey (Annie1007) <ananyadubey1007@gmail.com>
2026-04-14 16:10:42 +0530

    UC5: Multi-line command ard Hello

--- UC5.java
+++ UC5.java
@@ -1,4 +1,4 @@
-public class HelloApp {
+public class UC5 {
     public static void main(String[] args) {
         StringBuilder names = new StringBuilder();
 
@@ -13,6 +13,6 @@ public class HelloApp {
             }
         }
 
-        System.out.println("Hello, " + names + "!");
+        System.out.println("Hello, " + names.toString() + "!");
     }
 }
